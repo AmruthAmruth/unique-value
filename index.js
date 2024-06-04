b907ccdd0d3791d570c9771a0979b0ae209a82bd
Amruth Shyju <amruthshyju@gamil.com>
2024-06-04 15:02:09 +0530

    Final Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+function uniquevalue(existingValue,newValue){
+    let result;
+    if (existingValue.includes(newValue)) {
+       return result = true
+    } else {
+      return  result = false
+    }
+}
+
+module.exports= uniquevalue 
